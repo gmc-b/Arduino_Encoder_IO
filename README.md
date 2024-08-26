e015b63f55df2409a2abc1fc5c45c58002696ad6
Guilherme <gmcamargo.b@gmail.com>
2024-08-26 19:59:16 -0300

    Otimizações na função de leitura e alterações para ler + esquemático encoder

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# Leitor de Encoder com arduino
+# Leitor de Encoder incremental com arduino
 
-- Leitor de encoder incremental com canais A,B e Z.
+### Encoder:
   - Os canais A e B são utilizados como referência de cada passo de giro.
   - O canal Z é utilizado como referência para uma volta completa.
-
-- Placa utilizada: Arduino UNO
- 
+    
+  - Fios:
+      - Canal Z: Amarelo
+      - Canal A: Verde
+      - Canal B: Branco
+      - Vcc: Vermelho
+      - Gnd: Preto
+      - Shield
+        
+  ### Microcontrolador:
+   - Arduino Nano
+   - Código: [encoder_io.ino](https://github.com/gmc-b/Arduino_Encoder_IO/blob/main/encoder_io/encoder_io.ino)
+    
+  ### Esquemático
+  -   ![Esquemático](https://github.com/gmc-b/Arduino_Encoder_IO/blob/main/img/esquematico.png)
--- encoder_io/encoder_io.ino
+++ encoder_io/encoder_io.ino
@@ -5,12 +5,16 @@
 //
 // Placa: Arduino UNO
 // Interrupções:
-//    Para uma frequência de 50 Hz (T = 20 ms)
+//    Para uma frequência de 50  Hz (T = 20   ms)
+//    Para uma frequência de 80  Hz (T = 12.5 ms)
+//    Para uma frequência de 100 Hz (T = 10   ms)
 //    Clock sistema: 16 MHz
 //    Prescalar escolhido: 64
 //    Frequência timer 1: 16Mhz/64 = 250 KHz
 //    Pulse time = 1/250 Khz =  4 us 
-//    Contar até: 20 ms / 4 us = 5.000 (f = 50 Hz)
+//    - (f = 50  Hz) -> TIMER_COUNTER = 5000  (20   ms / 4 us)  
+//    - (f = 80  Hz) -> TIMER_COUNTER = 3125  (12.5 ms / 4 us)  
+//    - (f = 100 Hz) -> TIMER_COUNTER = 2500  (10   ms / 4 us)  
 
 
 
@@ -21,7 +25,7 @@ const short INPUT_PIN_ENCODER_Z = 2; // Sinal de entrada de referência de volta
 const short INPUT_PIN_ENCODER_A = 3;   // Sinal de entrada do canal A do encoder    // Apenas os pinos 2 e 3 do arduino UNO permitem interrupções, não alterar      
 const short INPUT_PIN_ENCODER_B = 4;   // Sinal de entrada do canal B do encoder   
 
-const int  ENCODER_RESOLUTION = 600;
+const int  ENCODER_RESOLUTION = 360;
 const int  TIMER_COUNTER      = 5000;
 
 
@@ -31,36 +35,34 @@ volatile char  direction = 0;
 volatile char  pin_state = 0;
 volatile int   counter   = 0;
 
-char serial_buffer     = 0;
 
-
-
-//////////////// Funções adicionais ////////////////
-void set_state(){   
+//////////////// Interrupções ////////////////
+void encoder_counter(){   
 
   direction = ( ( PIND + 0b00001000) & 0b00010000); // direction > 0 para Sentido positivo, direction = 0 para sentido negativo 
 
   if (direction){
     counter --; // Sentido anti-horário adotado como negativo
-    if (counter < 0) counter = ENCODER_RESOLUTION-1;
+    if (counter < 0)                    counter += ENCODER_RESOLUTION;
   }
   else{
     counter ++; // Sentido horário adotado como positivo
-    if (counter >= ENCODER_RESOLUTION) counter = 0;
+    if (counter > ENCODER_RESOLUTION)  counter -= ENCODER_RESOLUTION;
     
   }
+}
+
+void tare(){
+  counter = 0;
 }
 
 
 ISR(TIMER1_COMPA_vect){
   TCNT1  = 0;                  // Reseta o contador para a próxima interrupção
-  Serial.println(counter );
+  Serial.println(counter);
 }
 
 
-void tare(){
-  counter = 0;
-}
 
 
 //////////////////////////////// Setup ////////////////////////////////
@@ -71,14 +73,13 @@ void setup() {
   //////////////// Timer interrupt ////////////////
   cli();                      // Desativa interrupções para configuração
   
+
   TCCR1A = 0;                 // Atribui valor 0 para o registrador TCCR1A para desativar modo PWM
   TCCR1B = 0;                 // Atribui valor 0 para o registrador TCCR1B 
 
   TCCR1B |= 0b00000011;        // Atribuição de valor 011 (prescalar: 64) para os bits responsáveis pela definição de prescalar
   TIMSK1 |= 0b00000010;        // Atribuição de valor 10 para habilitar o modo comparador
   OCR1A   = TIMER_COUNTER;     // Define o valor de comparação para ativação da interrupção
-
-  sei();                      // Ativa novamente as interrupções
 
 
   //////////////// Definição de pinos ////////////////
@@ -88,8 +89,11 @@ void setup() {
   pinMode(INPUT_PIN_ENCODER_A, INPUT);
   pinMode(INPUT_PIN_ENCODER_B, INPUT);
   
-  //attachInterrupt(digitalPinToInterrupt(INPUT_PIN_ENCODER_Z), tare, FALLING);
-  attachInterrupt(digitalPinToInterrupt(INPUT_PIN_ENCODER_A), set_state, FALLING);
+  attachInterrupt(digitalPinToInterrupt(INPUT_PIN_ENCODER_Z), tare, FALLING);
+  attachInterrupt(digitalPinToInterrupt(INPUT_PIN_ENCODER_A), encoder_counter, FALLING);
+
+
+  sei();                      // Ativa novamente as interrupções
 
 }
 
